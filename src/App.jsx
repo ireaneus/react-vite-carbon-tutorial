d032af86980474dd61e04053d43e41ef66b49115
ireaneus <bwdave@protonmail.com>
2022-10-26 16:54:28 -0600

    5 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { Button, Content } from '@carbon/react';
+import { Content } from '@carbon/react';
 import TutorialHeader from './components/TutorialHeader';
 import './app.scss';
+import { Route, Routes } from 'react-router-dom';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
 
 function App() {
   return (
     <>
       <TutorialHeader />
       <Content>
-        <Button>Button</Button>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/repos" element={<RepoPage />} />
+        </Routes>
       </Content>
     </>
   );
