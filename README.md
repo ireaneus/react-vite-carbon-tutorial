3721179ff475250deadaf63b519c8c5721814a22
ireaneus <bwdave@protonmail.com>
2022-10-31 07:58:41 -0600

    5 files modified, 1 added

--- README.md
+++ README.md
@@ -4,4 +4,18 @@
 
 [Carbon Design System Tutorial](https://carbondesignsystem.com/developing/react-tutorial/step-1)
 
+## vite specific setting for Github or .env replacements for process.env variables
+
+Prefix for Vite variables 'VITE\_'
+
+```js
+const client = new ApolloClient({
+  cache: cache,
+  uri: 'https://api.github.com/graphql',
+  headers: {
+    authorization: `Bearer ${import.meta.env.VITE_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+  },
+});
+```
+
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-hwcyqs)
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import './index.scss';
 
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  cache: cache,
+  uri: 'https://api.github.com/graphql',
+  headers: {
+    authorization: `Bearer ${import.meta.env.VITE_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </ApolloProvider>
 );
